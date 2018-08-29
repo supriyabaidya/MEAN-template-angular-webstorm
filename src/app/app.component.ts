3737a5451ebfeff38118d5bbe3823928d12196bc
supriyabaidya <33324876+supriyabaidya@users.noreply.github.com>
2018-08-29 20:13:57 +0530

    Initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Http, Response} from '@angular/http';
+import {Observable} from 'rxjs';
+
+import 'rxjs-compat/add/operator/map';
+import 'rxjs-compat/add/operator/catch';
+
+import {RestApiService} from './rest-api.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +14,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  url = 'http://localhost:3030/test';
+  message: string;
+
+  constructor(private http: Http, private restApi: RestApiService) {
+  }
+
+  async serverTest() {
+    this.http.get(this.url)
+      .map((response: Response) => response.json())
+      .catch((err: Response) => {
+        return Observable.throw(err.json());
+      })
+      .subscribe(
+        data => this.message = Date() + 'Response: ' + data['message'],
+        error => console.error(error)
+      );
+  }
+
+  async serverTestNew() {
+    const dataReceived = await this.restApi.get(this.url);
+    this.message = Date() + ' Response new: ' + dataReceived['message'];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {HttpClientModule} from '@angular/common/http';
 
-import { AppComponent } from './app.component';
+import {HttpModule} from '@angular/http';
+
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {RestApiService} from './rest-api.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    NgbModule,
+    HttpClientModule,
+    HttpModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [RestApiService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
